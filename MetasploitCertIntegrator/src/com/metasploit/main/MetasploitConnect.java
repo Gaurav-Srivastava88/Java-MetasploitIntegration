d1e4c1324fcc373a6e5f38aba87efd615ed778ae
Gaurav Srivastava <gaurav.srivastava7@gmail.com>
2016-04-09 14:27:53 +0200

    SSH Version scanning from Mac Host eclipse project to Kali Metasploit framework using MSGRPC

--- MetasploitCertIntegrator/src/com/metasploit/main/MetasploitConnect.java
+++ MetasploitCertIntegrator/src/com/metasploit/main/MetasploitConnect.java
@@ -1,5 +1,7 @@
 package com.metasploit.main;
 
+import java.util.HashMap;
+
 import com.metasploit.connector.RpcConnection;
 
 public class MetasploitConnect {
@@ -10,6 +12,23 @@ public class MetasploitConnect {
 		int port = 55552;
 		boolean ssl = false;
 		
-		RpcConnection.getConn(username, password, host, port, ssl);
+		RpcConnection conn = RpcConnection.getConn(username, password, host, port, ssl);
+		Object[] params = {};
+		Object[] params1 = {"2", "use auxiliary/scanner/ssh/ssh_version\n"};
+		Object[] params2 = {"2", "set RHOSTS 192.168.1.8\n"};
+		Object[] params3 = {"2", "run\n"};
+		Object[] params4 = {"2"};
+		
+		System.out.println(conn.execute("core.module_stats", params));
+		
+		System.out.println(conn.execute("core.version", params));
+		//System.out.println(conn.execute("console.create", params));		
+		System.out.println(conn.execute("module.auxiliary", params));
+		System.out.println(conn.execute("console.write", params1));		
+		System.out.println(conn.execute("console.write", params2));		
+		System.out.println(conn.execute("console.write", params3));	
+		HashMap<String, String> jsonValue = (HashMap<String, String>) conn.execute("console.read", params4);
+		System.out.println(jsonValue.get("data"));
+		
 	}
 }
